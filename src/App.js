5c4365e6e620822a3f48bc7da4ce6e62a7433efd
Raddiya <raddiyasultana.612@gmil.com>
2022-04-04 22:35:20 +0600

    components add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import About from './Components/About/About';
+import Home from './Components/Home/Home'
+import Review from './Components/Review/Review';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Blog from './Components/Blog/Blog';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h2>hello</h2>
+      <h1>Welcome to my website</h1>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/review" element={<Review></Review>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+      </Routes>
+
+
     </div>
   );
 }
---
+++ src/Components/Review/Review.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Review = () => {
+    return (
+        <div>
+            <p>hello1</p>
+        </div>
+    );
+};
+
+export default Review;
