5f5f1ae464ac1374a94117a8dffb924284b32493
Raddiya <raddiyasultana.612@gmil.com>
2022-04-05 14:33:48 +0600

    header done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,17 +5,17 @@ import CoustomLink from '../CoustomLink/CoustomLink';
 const Header = () => {
     return (
         <div>
-            <h1 className='web-name'>Camera hub bd</h1>
+
             <nav className='navbar'>
+                <h1 className='web-name'>Camera hub bd</h1>
                 <CoustomLink to="/">Home</CoustomLink>
                 <CoustomLink to="/dashboard">Dashboard</CoustomLink>
                 <CoustomLink to="/review">Review</CoustomLink>
                 <CoustomLink to="/blog">Blog</CoustomLink>
                 <CoustomLink to="/about">About</CoustomLink>
-                
             </nav>
         </div>
-        
+
     );
 };
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,28 +3,29 @@ import Camera from '../image/dslr-camera.jpg'
 import { Link } from "react-router-dom";
 import useCard from "../Card/Card";
 import Review from "../Review/Review";
+import "./Home.css";
 
 const Home = () => {
     const [card, setCard] = useCard();
     return (
         <section>
-            <div>
-                <div>
-                    <img src={Camera} alt="Dslr" />
+            <div className='row container mt-4 align-items-center'>
+                <div className='camera col-lg-5 col-md-4' >
+                    <img className='img-fluid' src={Camera} alt="Dslr" />
                 </div>
-                <div>
-
+                 <div className='col-lg-7 col-md-8'>
+                An ideal travel camera is lightweight, versatile, and captures great images. Point-and-shoots are the smallest and least expensive option, but they have some limitations in terms of image quality. Mirrorless interchangeable-lens cameras have big...
                 </div>
             </div>
-            <div>
-                <p>Review</p>
-                <div className="card-compo">
+            <hr></hr>
+            <div>               
+                <h4>Coustomer review(3)</h4>
+                <div className="card">
                     {card.map(
-                        (item, index) => index < 3 && <Review product={item}></Review>
+                        (item, index) => index < 3 && <Review key={index} product={item}></Review>
                     )}
                 </div>
-                <div className=" d-flex justify-content-center"><Link className="reviewBtn" to="/review"> See more</Link>
-                </div>
+                <Link className="bg-primary text-white p-2 mb-2" to="/review"> See more</Link>            
             </div>
 
         </section >
